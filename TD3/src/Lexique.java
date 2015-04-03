a3f16d64ee8e6adb4f0af432b5d899c7bc33b087
crgarridos <cgarrido@etu.utc.fr>
2015-04-03 14:57:37 +0200

    add Main et demi-leintruc

--- TD3/src/Lexique.java
+++ TD3/src/Lexique.java
@@ -44,4 +44,41 @@ public class Lexique {
 		
 		return null;
 	}
+
+	public String[] levenshtein(String m1,String m2) {
+//		LevenshteinDistance(m A , m B )
+//			for i ← 0 to |m A |
+//				do dist[i, 0] = dist[i − 1, 0] + γ(X i , λ)
+//			for j ← 0 to |m B |
+//				do dist[0, j] = dist[0, j − 1] + γ(λ, Y i )
+//			for i ← 1 to |m A |
+//				do for j ← 1 to |m B |
+//					do d 1 = dist[i − 1, j − 1) + γ(X i , Y i )
+//						d 2 = dist[i − 1, j] + γ(X i , λ)
+//						d 3 = dist[i, j − 1] + γ(λ, Y j )
+//						dist[i, j] = min(d 1 , d 2 , d 3 )
+//			return dist[|m A |, |m B |]
+		int[][] dist = new int[m1.length()][m2.length()];
+		for (int i = 0; i < m1.length(); i++)
+			dist[i][0] = dist[i-1][0] + cout(1);
+		for (int j = 0; j < m1.length(); j++)
+			dist[0][j] = dist[0][j-1] + cout(1);
+		for (int i = 0; i < m1.length(); i++) {
+			for (int j = 0; j < m1.length(); j++) {
+				int d1 = dist[i-1][j-1] + cout(1);
+				int d2 = dist[i-1][j-1] + cout(1);
+				int d3 = dist[i-1][j-1] + cout(1);
+				dist[0][j] = dist[0][j-1] + cout(1);
+			}
+		}
+		return null;
+	}
+
+	private int cout(int i) {
+		// TODO Stub de la méthode généré automatiquement
+		return 0;
+	}
+	
+	
+	
 }
--- TD3/src/Main.java
+++ TD3/src/Main.java
@@ -2,14 +2,21 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 
+
+
 public class Main {
+	private static final String path_lexique = "lexique.txt";
+
 	public static void main(String[] args) {
+		Lexique lex = new Lexique(path_lexique);
 		Scanner in = new Scanner(System.in);
 		String s = in.nextLine();
 		StringTokenizer st = new StringTokenizer(s," ");
 		while(st.hasMoreElements()){
-			String str = (String) st.nextElement();
-			//TODO lexique...
+			String mot = (String) st.nextElement();
+			String lemme = lex.retrouve(mot.toLowerCase());
+//			if(lemme)
+			String[] candidats = lex.levenshtein(mot);
 		}
 	}
 }
