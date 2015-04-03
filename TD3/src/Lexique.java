1ad3e1959ef8f2c6a8c54aa2c978030ca3122fbb
LO17 Etudiant <lo17p016@smestaens24ap.sme.utc>
2015-04-03 14:23:33 +0200

    added lexique

---
+++ TD3/src/Lexique.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+
+public class Lexique {
+	
+	protected Map<String, String> lexique;
+	
+	public Lexique(String filePath){
+		this.lexique = new HashMap<String, String>();
+		
+		Scanner scanner;
+		try {
+			scanner = new Scanner(new File(filePath));
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+			return;
+		}
+		
+		// On boucle sur chaque champ detecté
+		while (scanner.hasNextLine()) {
+		    String line = scanner.nextLine();
+		 
+		    System.out.println(line);
+		    StringTokenizer st = new StringTokenizer(line, "\t");
+		    if(st.countTokens() == 2){
+		    	String mot = st.nextToken();
+		    	String lemme = st.nextToken();
+		    	this.lexique.put(mot, lemme);
+		    }
+		}
+		
+		scanner.close();
+	}
+
+	public String retrouve(String mot){
+		if(lexique.containsKey(mot)){
+			return lexique.get(mot);
+		}
+		
+		return null;
+	}
+}
