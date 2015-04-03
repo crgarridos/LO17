5d076d3e900c97b23eb164f7a1a2d3fda02e1348
crgarridos <cgarrido@etu.utc.fr>
2015-04-03 15:32:51 +0200

    fix Levenshtein alg

--- TD3/src/Lexique.java
+++ TD3/src/Lexique.java
@@ -63,25 +63,35 @@ public class Lexique {
 //			return dist[|m A |, |m B |]
 
 		int[][] dist = new int[m1.length()+1][m2.length()+1];
-		for (int i = 0; i < m1.length(); i++)
-			dist[i][0] = dist[i-1][0] + cout(m1.charAt(i),null);
-		for (int j = 0; j < m1.length(); j++)
-			dist[0][j] = dist[0][j-1] + cout(null,m2.charAt(j));
-		for (int i = 0; i < m1.length(); i++) {
-			for (int j = 0; j < m1.length(); j++) {
-				int d1 = dist[i-1][j-1] + cout(m1.charAt(i),m2.charAt(i));
-				int d2 = dist[i-1][j] + cout(m1.charAt(i),null);
-				int d3 = dist[i][j-1] + cout(null,m2.charAt(j));
+		dist[0][0]=0;
+		for (int i = 1; i <= m1.length(); i++)
+			dist[i][0] = dist[i-1][0] + cout(m1.charAt(i-1),null);
+			
+		for (int j = 1; j <= m2.length(); j++)
+			dist[0][j] = dist[0][j-1] + cout(null,m2.charAt(j-1));
+			
+		for (int i = 1; i <= m1.length(); i++) {
+			for (int j = 1; j <= m2.length(); j++) {
+				int d1 = dist[i-1][j-1] + cout(m1.charAt(i-1),m2.charAt(i-1));
+				int d2 = dist[i-1][j] + cout(m1.charAt(i-1),null);
+				int d3 = dist[i][j-1] + cout(null,m2.charAt(j-1));
+				
 				dist[i][j] = Math.min(Math.min(d1, d2),d3);
 			}
+		}
+		for (int i = 0; i < m1.length(); i++) {
+			for (int j = 0; j < m2.length(); j++) {
+				System.out.print(dist[i][j]);
+			}
+			System.out.println();
 		}
 		return dist[m1.length()][m2.length()];
 	}
 
 	private int cout(Character x, Character y) {
 		if(x == null || y == null || !x.equals(y))
-			return 0;
-		else return 1;
+			return 1;
+		else return 0;
 	}
 	
 	
--- TD3/src/Main.java
+++ TD3/src/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Lexique lex = new Lexique(path_lexique);
+		System.out.println(lex.levenshtein("cat", "catcat"));
 		Scanner in = new Scanner(System.in);
 		String s = in.nextLine();
 		StringTokenizer st = new StringTokenizer(s," ");
